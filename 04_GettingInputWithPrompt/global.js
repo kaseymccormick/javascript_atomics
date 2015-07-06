e7678b7f9195610a541032e02034f83d21d54a69
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 14:13:32 -0500

    updating the questions needed

--- 04_GettingInputWithPrompt/global.js
+++ 04_GettingInputWithPrompt/global.js
@@ -1,3 +1,25 @@
 //setting variable using a propt of the user//
-var name = prompt("What is your name", "Type you name here");
-alert("Hi " + name + "\nHope you are enjoying JavaScript!");
+var userName = prompt("What is your name", "Type you name here");
+
+
+alert("your name is" + userName)
+console.log ("user's name is " + userName)
+
+alert("your name is" + userName)
+console.log ("user's name is " + userName)
+
+alert("your name is" + userName)
+console.log ("user's name is " + userName)
+
+alert("your name is" + userName)
+console.log ("user's name is " + userName)
+
+alert("your name is" + userName)
+console.log ("user's name is " + userName)
+
+
+Ask the user (using prompt()) for their name, then assign that value to a well-named variable.
+Ask the user (using prompt()) for their age, then assign that value to a well-named variable.
+Ask the user (using prompt()) for their hometown, then assign that value to a well-named variable.
+Ask the user (using prompt()) for their favorite number, then assign that value to a well-named variable.
+Output each of those values using both console.log() and alert(). You can output the value itself with no context around it, but it's a better decision to use each of those values in a sentence. For example: "Your name is Aloysius."
