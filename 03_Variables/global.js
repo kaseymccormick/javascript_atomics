af9eb5b448b5a2c4242e696cdc5e3dae27adae73
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 13:33:14 -0500

    completed variable exersize

---
+++ 03_Variables/global.js
@@ -0,0 +1,20 @@
+//defining variables//
+var age = 7.5;
+var name = "Marty";
+var hairColor = "orange";
+
+//alerting the user of the variables//
+alert("my name is " + name)
+alert("I am " + age + " years old");
+alert("my hair color is "+ hairColor);
+
+
+//defining variables//
+var ages;
+var names = "Marty";
+var hairColors = "orange";
+
+//alerting the user of the variables//
+alert("my name is " + names)
+alert("I am " + ages + " years old");
+alert("my hair color is "+ hairColors);
