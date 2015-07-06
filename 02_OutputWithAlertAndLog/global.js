fd5002d6bcac99166aee9b75a18cc1db4bcb5659
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 11:46:45 -0500

    finished adding console.log

--- 02_OutputWithAlertAndLog/global.js
+++ 02_OutputWithAlertAndLog/global.js
@@ -2,10 +2,15 @@
 alert("Hello! I'm Lucy.");
 
 // output of informaiton//
-console.log(5 + 6);
+//running into issues on how to use the console.log command. document write works as it 'puts' to the page. console.log seems to give me a shrug.//
+//console.log 'puts' the info into the console part of web developer doesn't put to the screen :(//
 
-
-
-
+document.write("look in the console tab of web developer");
+console.log("I take 2 naps a day");
+console.log("I live in a treehouse");
+console.log("I used to work at a chocolate factory.");
+console.log("Ricky is my cat's name.");
+console.log("I'm a fan of the 2014 film with the same name.");
 
 //alert of leaving//
+alert("it was nice talking to you. See you when I see you.");
