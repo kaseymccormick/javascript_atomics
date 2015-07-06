53ea490965bae75cf19ad9103eee82da3cff4a8c
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 15:54:56 -0500

    finished step

--- 05_BasicMath/global.js
+++ 05_BasicMath/global.js
@@ -8,12 +8,26 @@ var userInput3 = prompt("Please enter your third number");
 alert("your numbers are" + userInput1 + userInput2 + userInput3);
 console.log ("users numbers are" + userInput1 + userInput2 + userInput3);
 
+//turning variables into intigers//
 var userNumber1 = parseInt(userInput1, 10);
 var userNumber2 = parseInt(userInput2, 10);
 var userNumber3 = parseInt(userInput3, 10);
 
+//varables set for math//
 var sum = (userNumber1 + userNumber2 + userNumber3);
 var difference = (userNumber1 - userNumber2 - userNumber3);
-console.log(userInput1 + "+" userInput2 + "+" userInput3 + "=" + sum);
+var product = (userNumber1 * userNumber2 * userNumber3);
+var firstDIVsec = (userInput1 + "/" + userInput2);
 
-console.log(userInput1 + "-" userInput2 + "-" userInput3 + "=" + (userInput1 - userInput2 - userInput3));
+//outputting variable math//
+console.log(userInput1 + "+" + userInput2 + "+" + userInput3 + "=" + sum);
+
+console.log(userInput1 + "-" + userInput2 + "-" + userInput3 + "=" + difference);
+
+console.log(userInput1 + "*" + userInput2 + "*" + userInput3 + "=" + product);
+
+console.log(userInput1 + "/" + userInput2 + "=" + difference);
+
+console.log(++userNumber1);
+
+console.log(--userNumber2);
