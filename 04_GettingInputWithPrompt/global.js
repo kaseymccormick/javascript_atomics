12226190ac28aaaeee5caa3ed618480fe9d125a6
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 14:18:02 -0500

    finished exersize

--- 04_GettingInputWithPrompt/global.js
+++ 04_GettingInputWithPrompt/global.js
@@ -1,25 +1,20 @@
 //setting variable using a propt of the user//
-var userName = prompt("What is your name", "Type you name here");
+var userName = prompt("What is your name?", "Type your name here");
+var userAge = prompt("How old are you?", "Type your age here");
+var userTown = prompt("What is your hometown?", "Type your hometown here");
+var userNumber = prompt("What is your favorite number?", "Type your favorite number here");
+
+//iterating the user data back in prompts and via console
+alert("your name is" + userName);
+console.log ("user's name is " + userName);
+
+alert("your age is" + userAge);
+console.log ("user's age is " + userAge);
+
+alert("your hometown is" + userTown);
+console.log ("user's hometown is " + userTown);
+
+alert("your favorite number is" + userNumber);
+console.log ("user's favorite number is " + userNumber);
 
 
-alert("your name is" + userName)
-console.log ("user's name is " + userName)
-
-alert("your name is" + userName)
-console.log ("user's name is " + userName)
-
-alert("your name is" + userName)
-console.log ("user's name is " + userName)
-
-alert("your name is" + userName)
-console.log ("user's name is " + userName)
-
-alert("your name is" + userName)
-console.log ("user's name is " + userName)
-
-
-Ask the user (using prompt()) for their name, then assign that value to a well-named variable.
-Ask the user (using prompt()) for their age, then assign that value to a well-named variable.
-Ask the user (using prompt()) for their hometown, then assign that value to a well-named variable.
-Ask the user (using prompt()) for their favorite number, then assign that value to a well-named variable.
-Output each of those values using both console.log() and alert(). You can output the value itself with no context around it, but it's a better decision to use each of those values in a sentence. For example: "Your name is Aloysius."
