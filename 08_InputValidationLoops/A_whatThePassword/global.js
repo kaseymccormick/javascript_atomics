070a924e8256a7fab8245b51015e0ec0c0e9480e
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 14:27:55 -0500

    setting up file structure

---
+++ 08_InputValidationLoops/A_whatThePassword/global.js
@@ -0,0 +1,12 @@
+var password="timey wimey"
+
+var passwordGuess = prompt("password needed, please enter password", "enter password guess here");
+
+if(password === passwordGuess)
+{
+  alert("success");
+}
+else if (password !== passwordGuess)
+{
+alert("sorry that's not it")
+}
