d84773ab6a57fc0eb85f148fa971ee9e4c929a1c
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-06 22:22:46 -0500

    updating answers to questions

---
+++ 06_IfStatments/6C_guess_a_number/global.js
@@ -0,0 +1,13 @@
+
+var magicNumber= "13"
+
+var numberGuess = prompt("password needed, please enter password", "enter password guess here");
+
+if( magicNumber === numberGuess)
+{
+  alert("success");
+}
+else if ( magicNumber !== numberGuess)
+{
+alert("sorry that's not it")
+}
