737d557c015edd3e6fb0459a6a15b49259472dd3
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 14:42:51 -0500

    finished task of password loop

--- 08_InputValidationLoops/A_whatThePassword/global.js
+++ 08_InputValidationLoops/A_whatThePassword/global.js
@@ -1,12 +1,10 @@
+
+
 var password="timey wimey"
-
-var passwordGuess = prompt("password needed, please enter password", "enter password guess here");
-
-if(password === passwordGuess)
-{
-  alert("success");
-}
-else if (password !== passwordGuess)
-{
-alert("sorry that's not it")
-}
+while (passwordGuess !== password) {
+    var passwordGuess = prompt("password needed, please enter password", "enter password guess here");
+    if(passwordGuess === password){
+      alert("you guessed the password!");
+    }
+  }
+   
