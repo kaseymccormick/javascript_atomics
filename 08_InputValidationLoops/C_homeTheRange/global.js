b3a66a87de8dbd2fdcdbbbab4e576a442d8162a2
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-07 15:36:15 -0500

    finished home on the range and guess the number

---
+++ 08_InputValidationLoops/C_homeTheRange/global.js
@@ -0,0 +1,8 @@
+//inclination was to use an array and see if the user's input was in the array. Hello tougher. Had to look at an example//
+//set variable to intiger version of the user's input radix 10//
+  var userNumber = parseInt(prompt("I need a number between 1 and 8"), 10);
+  //loop while the user number input is greaterthan or equal to 1 and less than or equal to 8//
+    while (userNumber >= 1 && userNumber <= 8) {
+      //set variable to intiger version of the user's input radix 10//
+      userNumber = parseInt(prompt("I need a number between 1 and 8"), 10);
+    }
